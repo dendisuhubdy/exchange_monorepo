a3bcc8adf8fa5bad8578c3227537c97562bb39e5
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2018-07-02 10:29:33 -0400

    removing quickfast because mFast has better performance, implementing Liquibook order matching system in matching-engine-server, adjusting order management system

--- build_all.sh
+++ build_all.sh
@@ -1,43 +1,19 @@
 # Build all repositories
 
 # Build clearning server 
-mkdir -p clearing/build
-cd clearning/build 
-cmake ..
-make -j16 
-cd ../../
+./clearing/build_all.sh
 
 # Build database server 
-mkdir -p database-server/build
-cd database-server/build 
-cmake ..
-make -j16
-cd ../../ 
+./database_server/build_all.sh
 
 # Build quickfix message server
-mkdir -p quickfast/build
-cd quickfast/build 
-cmake ..
-make -j16
-cd ../../ 
+./quickfix/build_all.sh
 
 # Build quickfast broadcast server 
-mkdir -p quickfast/build
-cd quickfast/build 
-cmake ..
-make -j16
-cd ../../ 
+./quickfast/build_all.sh
 
 # Build order management server 
-mkdir -p order_management_system/build
-cd order_management_system/build 
-cmake ..
-make -j16
-cd ../../ 
+./order_management_system/build_all.sh
 
 # Build matching engine server 
-mkdir -p matching-engine-server/build
-cd matching-engine-server/build 
-cmake ..
-make -j16
-cd ../../ 
+./matching_engine_server/build_all.sh
