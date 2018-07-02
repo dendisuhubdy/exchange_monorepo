42249e9403735fb66584aea2af21d2f77a95a730
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2018-07-02 10:49:12 -0400

    adding mongo-c-driver, adding mongo-cxx-driver, adding quickfix

--- build_all.sh
+++ build_all.sh
@@ -1,16 +1,13 @@
 # Build all repositories
 
+# Build dependencies
+
+
 # Build clearning server 
 ./clearing/build_all.sh
 
 # Build database server 
 ./database_server/build_all.sh
-
-# Build quickfix message server
-./quickfix/build_all.sh
-
-# Build quickfast broadcast server 
-./quickfast/build_all.sh
 
 # Build order management server 
 ./order_management_system/build_all.sh
